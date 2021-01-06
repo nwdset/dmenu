8a5d6517af31dad1de69370d2a2dfe7bdfb67c00
Jeff <jeffli.1644@gmail.com>
2021-01-05 23:00:28 -0800

    change colors to match theme

--- config.h
+++ config.h
@@ -4,13 +4,13 @@
 static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrains Mono:size=7"
 };
 static const char *prompt = NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*                  fg         bg       */
-	[SchemeNorm]    = { "#bbbbbb", "#222222" },
-	[SchemeSel]     = { "#eeeeee", "#005577" },
+	[SchemeNorm]    = { "#c5c8c6", "#1d1f21"},
+	[SchemeSel]     = { "#c8ccd4", "#3c3836"},
 	[SchemeOut]     = { "#000000", "#00ffff" },
 	[SchemeBorder]  = { "#000000", "#005577" }, /* fg unused */
 };
